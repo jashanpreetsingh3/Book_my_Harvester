b9666d450e06b575682317e4f47f966dab2e7f20
Jashanpreet Singh <jashanpreet0433@gmail.com>
2022-04-12 21:29:14 +0530

    create-backend-node-server

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Shaktiman Paddy Master 3776',
+      slug: 'harvester1',
+      category: 'For Rent Harvesters',
+      image: '/images/p1.jpg',
+      price: '1300 per hour',
+      rating: 4.5,
+      description: 'Reliable combine for rent',
+    },
+    {
+      name: 'Kartar 4000',
+      slug: 'harvester2',
+      category: 'For Rent Harvesters',
+      image: '/images/p2.jpg',
+      price: '2500 per hour',
+      rating: 4,
+      description: 'Reliable combine for rent',
+    },
+    {
+      name: 'Dashmesh 9100 maize',
+      slug: 'harvester3',
+      category: 'For Rent Harvesters',
+      image: '/images/p3.jpg',
+      price: '2000 per hour',
+      rating: 4,
+      description: 'Reliable combine for rent',
+    },
+    {
+      name: 'Preet 987',
+      slug: 'harvester4',
+      category: 'For Rent Harvesters',
+      image: '/images/p4.jpg',
+      price: '1700 per hour',
+      rating: 4,
+      description: 'Reliable combine for rent',
+    },
+  ],
+};
+
+export default data;
