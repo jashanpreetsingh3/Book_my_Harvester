541195c27785fdf05afa9da0e609e7e27dac667b
Jashanpreet Singh <jashanpreet0433@gmail.com>
2022-04-12 18:31:18 +0530

    added-images-and-style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Book my Harvester</a>
       </header>
-      <main>list machinery</main>
+      <main>
+        <h1>Harvesters for rent</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product">
+              <a href={`/products/${product.name}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/products/${product.name}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>₹{product.price}</strong>
+                </p>
+                <button>Book now</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
