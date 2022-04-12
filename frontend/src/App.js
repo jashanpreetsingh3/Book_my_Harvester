4ed9a1bf1a97b0c427545ff15659f5122e1c8982
Jashanpreet Singh <jashanpreet0433@gmail.com>
2022-04-12 17:18:30 +0530

    Step-1-Create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Book my Harvester</a>
+      </header>
+      <main>list machinery</main>
+    </div>
+  );
+}
+
+export default App;
