f310930e772f754a37d9a4f6cf4bba6a2d352e0d
Jashanpreet Singh <jashanpreet0433@gmail.com>
2022-04-14 20:38:01 +0530

    update-product-screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Products';
+import { Helmet } from 'react-helmet-async';
 //import data from '../data';
 
 const reducer = (state, action) => {
@@ -41,6 +42,9 @@ function HomeScreen() {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>Book my Harvester</title>
+      </Helmet>
       <h1>Featured Harvesters</h1>
       <div className="products">
         {loading ? (
