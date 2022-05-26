1bb4364a4b35f0610b274d8ad89ec509cb66280f
Jashanpreet Singh <jashanpreet0433@gmail.com>
2022-05-26 19:22:28 +0530

    terms-update

--- frontend/src/screens/TermsScreen.js
+++ frontend/src/screens/TermsScreen.js
@@ -6,10 +6,29 @@ export default function TermsScreen() {
       <h1>Terms & Conditions</h1>
       <ol>
         <li>
-          Prices displayed are per acre. Final payment will be calculated by
-          multiplying the base price with total acres you want to harvest.
+          <strong>
+            Prices displayed are per acre. Final payment will be calculated by
+            multiplying the base price with total acres you want to harvest.
+          </strong>
         </li>
-        <li>Total payment can be paid after the job is completed.</li>
+        <li>
+          <strong>Total payment can be paid after the job is completed.</strong>
+        </li>
+        <li>
+          <strong>
+            Amount paid while booking is refundable within one day.
+          </strong>
+        </li>
+        <li>
+          <strong>Refund will be subject to 10% refund fee.</strong>
+        </li>
+        <li>
+          <strong>
+            If the harvester faces a problem or breaks down during harvesting,
+            it is the rsposibility of the machine owner to get it in working
+            condition again.
+          </strong>
+        </li>
       </ol>
     </div>
   );
